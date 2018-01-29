9b815fac6beb11ac707d6b4f53c15d9e48a44cd6
Kazuya <fjky9132@gmail.com>
2018-01-29 11:35:46 -0800

    modify dev UI

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -23,7 +23,7 @@
             <div class="columns medium-3" v-if="transcript.has_content">
 
             <div class="card" >
-              <table >
+              <table bgcolor='#607780' >
                 <tr>
                   <td></td>
                   <td>
@@ -40,30 +40,7 @@
             </div>
 
 
-            <div class="columns medium-3" v-for="i in transcript.entities.length">
 
-            <div class="card" >
-              <table>
-              <font color='black'>
-                <tr>
-                  <td></td>
-                  <td>
-                    <h5> Extracted Entity Information {{i}} </h5>
-                  </td>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td>
-                    <p> name : {{ transcript.entities[i-1].name }} </p>
-                    <p> category : {{ transcript.entities[i-1].category }} </p>
-                  </td>
-                </tr>
-
-              </font>
-              </table>
-            </div>
-
-            </div>
 
             <div class="columns medium-3" v-for="i in transcript.related_contents.length">
 
@@ -81,11 +58,11 @@
                 <tr>
                   <td></td>
                   <td>
-                    <p> title : {{ transcript.related_contents[i-1].title }} </p>
-                    <p> desc : {{ transcript.related_contents[i-1].desc }} </p>
+                    <p> Title : {{ transcript.related_contents[i-1].title }} </p>
+                    <p> Desc : {{ transcript.related_contents[i-1].desc }} </p>
                     <div class="columns medium-3" v-if="transcript.related_contents[i-1].condition">
-                    <p> search  service : {{transcript.related_contents[i-1].condition.service }}</p>
-                    <p> search words : {{ transcript.related_contents[i-1].condition.word }}</p>
+                    <p> Search  Service : {{transcript.related_contents[i-1].condition.service }}</p>
+                    <p> Search Words : {{ transcript.related_contents[i-1].condition.word }}</p>
                     </div>
                   </td>
                 </tr>
@@ -105,9 +82,13 @@
                     <p> Created_at : {{transcript.created_at}} </p>
                     <p> User ID : {{ transcript.user.facebook_id }} </p>
                     <p> Conversation State :  {{ transcript.context.state }} </p>
+                    <div class="columns medium-3" v-for="i in transcript.entities.length">
+                      <p> Recognized Entity {{i}} : { name : {{ transcript.entities[i-1].name }} , category : {{ transcript.entities[i-1].category }} } </p>
+                    </div>
                   </td>
                   <td></td>
                 </tr>
+
               </font>
               </table>
             </div>
