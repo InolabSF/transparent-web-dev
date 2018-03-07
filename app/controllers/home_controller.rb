45e59388490400c3138c65d8e50e1cef9a5d69f8
Kazuya <fjky9132@gmail.com>
2018-03-07 13:49:17 -0800

    change background image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,16 +4,16 @@ class HomeController < ApplicationController
   end
 
   def top
-    render :file => "home/web/dest/index"
+    render :file => "home/web/dest/top"
   end
 
   def demo
     @wall_id = 1
-    render :file => "home/front/dest/index"
+    render :file => "home/front/dest/wall"
   end
 
   def demo_ja
     @wall_id = 2
-    render :file => "home/front/dest/index"
+    render :file => "home/front/dest/wall"
   end
 end
