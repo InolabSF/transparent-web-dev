c310831f82b72d3641e9968fa7765af40fe9cfde
Kazuya <fjky9132@gmail.com>
2018-03-07 18:08:32 -0800

    merge front file

--- front/src/_config/data.js
+++
@@ -1,9 +0,0 @@
-
-"use strict"
-
-module.exports = {
-    twitter: 'https://twitter.com/CloudBabyLove',
-    facebook: 'https://www.facebook.com/DOKI-DOKI-INC-1519179441630945/',
-    medium: 'https://medium.com/@dokidoki',
-    mail: 'mailto:hello@d0kid0ki.com'
-};
