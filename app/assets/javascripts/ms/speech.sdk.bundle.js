681876ad385f93eb50dccf6478336b076652f64d
Kazuya <fjky9132@gmail.com>
2018-04-07 16:23:45 -0700

    add error handler

--- app/assets/javascripts/ms/speech.sdk.bundle.js
+++ app/assets/javascripts/ms/speech.sdk.bundle.js
@@ -1758,6 +1758,9 @@ var WebsocketMessageAdapter = /** @class */ (function () {
             _this.sendMessageQueue.DrainAndDispose(function (pendingSendItem) {
                 pendingSendItem.SendStatusDeferral.Reject(closeReason);
             }, closeReason);
+
+            alert("Session Closed. Please Restart.");
+            killMic();
         };
         this.ProcessSendQueue = function () {
             _this.sendMessageQueue
