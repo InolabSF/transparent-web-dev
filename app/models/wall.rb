7c0fe9c82bce40ea907a98333b4b5690519819df
Kazuya <fjky9132@gmail.com>
2018-04-11 18:31:50 +0900

    change db for management

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -1,6 +1,6 @@
 class Wall < ApplicationRecord
   has_many :transcripts
-  has_many :configs, through: :config_group
   has_many :config_groups
+  has_many :configs, through: :config_groupsut
   accepts_nested_attributes_for :config_groups
 end
