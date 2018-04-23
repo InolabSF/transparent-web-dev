0be9e01132652c756c83ce0200ab17ecc94bdfeb
Kazuya <fjky9132@gmail.com>
2018-04-23 22:34:37 +0900

    modify mic error

--- app/assets/javascripts/ms/speech.sdk.bundle.js
+++ app/assets/javascripts/ms/speech.sdk.bundle.js
@@ -1758,10 +1758,6 @@ var WebsocketMessageAdapter = /** @class */ (function () {
             _this.sendMessageQueue.DrainAndDispose(function (pendingSendItem) {
                 pendingSendItem.SendStatusDeferral.Reject(closeReason);
             }, closeReason);
-
-            // edited
-            alert("Sorry, Session Closed. Please Restart.");
-            killMic();
         };
         this.ProcessSendQueue = function () {
             _this.sendMessageQueue
