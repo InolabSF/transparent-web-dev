35b565653a265ed2e9496e6003f8e8a5a831074e
kzya32 <kzya32@gmail.com>
2018-09-05 12:19:12 -0700

    delete legacy tables

--- app/models/transcript.rb
+++ app/models/transcript.rb
@@ -2,7 +2,6 @@ class Transcript < ApplicationRecord
   validates :text, presence: true
   belongs_to :wall
   # belongs_to :user
-  has_one :context
   has_many :entities
   has_many :with_words
   has_many :searches
