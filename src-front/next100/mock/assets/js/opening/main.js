e7863ff3c20385d6d80dedc9fff366a85fe7a51c
nakashima <nakashima@konel.jp>
2018-10-22 03:35:46 +0900

    待機画面の旧ファイル削除

--- src-front/next100/mock/assets/js/opening/main.js
+++ src-front/next100/mock/assets/js/opening/main.js
@@ -2,185 +2,268 @@
 
   window.addEventListener('load', () => {
 
-    // 汎用変数の宣言
-    let width = window.innerWidth; // ブラウザのクライアント領域の幅
-    let height = window.innerHeight; // ブラウザのクライアント領域の高さ
-    let targetDOM = document.getElementById('webgl'); // スクリーンとして使う DOM
+			var camera, scene, renderer;
+			var cube, sphere, torus, material;
 
-    // three.js 定義されているオブジェクトに関連した変数を宣言
-    let scene; // シーン
-    let camera; // カメラ
-    let renderer; // レンダラ
-    let axis; //ガイド
-    let grid; //ガイド
-    let directional;
-    let ambient;
-    let zoomVal = 0;
+			var count = 0, cubeCamera1, cubeCamera2;
 
-    // 各種パラメータを設定するために定数オブジェクトを定義
-    let CAMERA_PARAMETER = { // カメラに関するパラメータ
-      fovy: 90,
-      aspect: width / height,
-      near: 0.1,
-      far: 1000.0,
-      x: 0, // + 右 , - 左
-      y: 30, // + 上, - 下
-      z: 0, // + 手前, - 奥
-      lookAt: new THREE.Vector3(0.0, 0.0, 0.0) //x,y,z
-    };
-    let RENDERER_PARAMETER = { // レンダラに関するパラメータ
-      clearColor: 0xffffff, //背景のリセットに使う色
-      width: width,
-      height: height
-    };
+			var lon = 0, lat = 0;
+			var phi = 0, theta = 0;
 
-    let LIGHT_PARAMETER = {
-      directional: {
-        positionX: -0.5,
-        positionY: 4,
-        positionZ: 3
-      },
-      ambient: {
-        positionY: 1
-      }
-    }
+			var targetDOM = document.getElementById('webgl');
 
-    scene = new THREE.Scene();
+			var textureLoader = new THREE.TextureLoader();
 
-    camera = new THREE.PerspectiveCamera(
-      CAMERA_PARAMETER.fovy,
-      CAMERA_PARAMETER.aspect,
-      CAMERA_PARAMETER.near,
-      CAMERA_PARAMETER.far
-    );
+			textureLoader.load( './assets/img/opening/scene02.jpg', function ( texture ) {
 
-    camera.position.x = CAMERA_PARAMETER.x;
-    camera.position.y = CAMERA_PARAMETER.y;
-    camera.position.z = CAMERA_PARAMETER.z;
-    camera.lookAt(CAMERA_PARAMETER.lookAt); //注視点（どこをみてるの？）
+				texture.mapping = THREE.UVMapping;
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(new THREE.Color(RENDERER_PARAMETER.clearColor));
-    renderer.setSize(RENDERER_PARAMETER.width, RENDERER_PARAMETER.height);
-    renderer.shadowMap.enabled = true;
+				init( texture );
+				animate();
 
-    //renderer.shadowMap.enabled = true; //影を有効
-    targetDOM.appendChild(renderer.domElement); //canvasを挿入する
+			} );
 
-    controls = new THREE.OrbitControls(camera, render.domElement);
-    controls.autoRotate = true;
-    controls.minDistance = 30;
-    controls.maxDistance = 30;
+			function init( texture ) {
 
-    //ライト
-    directional = new THREE.DirectionalLight(0xffffff);
-    ambient = new THREE.AmbientLight(0xffffff, 0.25);
+				camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
 
-    directional.castShadow = true;
+				scene = new THREE.Scene();
 
+				var mesh = new THREE.Mesh( new THREE.SphereBufferGeometry( 500, 32, 16 ), new THREE.MeshBasicMaterial( { map: texture } ) );
+				mesh.geometry.scale( 1, 1,-  1 );
+				mesh.rotation.z = Math.PI / 2 / 2;
+				scene.add( mesh );
 
-    directional.position.y = LIGHT_PARAMETER.directional.positionY;
-    directional.position.z = LIGHT_PARAMETER.directional.positionZ;
-    directional.position.x = LIGHT_PARAMETER.directional.positionX;
-    ambient.position.y = LIGHT_PARAMETER.ambient.positionY;
+				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				//renderer.setPixelRatio( window.devicePixelRatio ); //chrome TEST fit size
+				renderer.setSize( window.innerWidth, window.innerHeight );
 
-    //directional.castShadow = true;
-    directional.shadow.mapSize.width = 800;
-    directional.shadow.mapSize.height = 800;
-    scene.add(directional);
-    scene.add(ambient);
+				cubeCamera1 = new THREE.CubeCamera( 1, 1000, 256 );
+				cubeCamera1.renderTarget.texture.minFilter = THREE.LinearMipMapLinearFilter;
+				scene.add( cubeCamera1 );
 
-    // axis = new THREE.AxisHelper(1000);
-    // axis.position.set(0, 0, 0);
-    // scene.add(axis);
+				cubeCamera2 = new THREE.CubeCamera( 1, 1000, 256 );
+				cubeCamera2.renderTarget.texture.minFilter = THREE.LinearMipMapLinearFilter;
+				scene.add( cubeCamera2 );
 
-    // // グリッドのインスタンス化
-    // grid = new THREE.GridHelper(100, 50);
+				targetDOM.appendChild(renderer.domElement);
 
-    // //グリッドオブジェクトをシーンに追加する
-    // scene.add(grid);
+				//
 
-    const PATH = './assets/img/opening/';
-    const FORMAT = '.jpg';
-    let urls = [
-      PATH + 'posx' + FORMAT,
-      PATH + 'negx' + FORMAT,
-      PATH + 'posy' + FORMAT,
-      PATH + 'negy' + FORMAT,
-      PATH + 'posz' + FORMAT,
-      PATH + 'negz' + FORMAT,
-    ];
+				var loader = new THREE.SVGLoader();
+				loader.load( './assets/img/opening/text01.svg', function ( paths ) {
 
-    let cubeTextureLoader = new THREE.CubeTextureLoader();
-    cubeTextureLoader.load(urls, function (cubeTexture) {
+					var group01 = new THREE.Group();
+					group01.scale.multiplyScalar( 0.2 );
+					group01.position.x = -250;
+					group01.position.y = 250;
+					group01.position.z = 240;
+					group01.scale.y *= -1;
+					group01.rotation.y = 1.525;
 
-      createSkyBox(cubeTexture);
+					for ( var i = 0; i < paths.length; i ++ ) {
 
-      createSphere(cubeTexture);
+						var path = paths[ i ];
 
-      //createSphere2(cubeTexture);
+						var material = new THREE.MeshBasicMaterial( {
+							color: path.color,
+							side: THREE.DoubleSide,
+							depthWrite: false
+						} );
 
-    });
+						var shapes = path.toShapes( true );
 
-    //skyboxを作成
-    function createSkyBox(cubeTexture) {
-      let shader = THREE.ShaderLib['cube'];
-      shader.uniforms['tCube'].value = cubeTexture;
+						for ( var j = 0; j < shapes.length; j ++ ) {
 
-      let material = new THREE.ShaderMaterial({
-        fragmentShader: shader.fragmentShader,
-        vertexShader: shader.vertexShader,
-        uniforms: shader.uniforms,
-        depthWrite: false,
-        side: THREE.BackSide
-      });
+							var shape = shapes[ j ];
 
-      let skybox = new THREE.Mesh(new THREE.BoxGeometry(200, 200, 200), material);
+							var geometry = new THREE.ShapeBufferGeometry( shape );
+							var mesh = new THREE.Mesh( geometry, material );
 
-      scene.add(skybox);
-    }
+							group01.add( mesh );
 
-    //球を作成(反射マッピング)
-    function createSphere(cubeTexture) {
-      var geometry = new THREE.SphereGeometry(12, 60, 60);
-      var torusMaterial = new THREE.MeshPhongMaterial({
-        color: 0xCCCCCC,
-        envMap: cubeTexture,
-        reflectivity: 0.9
-      });
-      mesh = new THREE.Mesh(geometry, torusMaterial);
-      scene.add(mesh);
-    }
+						}
 
-    //球を作成(屈折マッピング)
-    function createSphere2(cubeTexture) {
-      cubeTexture.mapping = THREE.CubeRefractionMapping;
+					}
 
-      var geometry = new THREE.SphereGeometry(12, 60, 60);
-      var torusMaterial = new THREE.MeshPhongMaterial({
-        color: 0xFFFFFF,
-        envMap: cubeTexture,
-        refractionRatio: 0.9,
-        reflectivity: 0.9
-      });
-      mesh = new THREE.Mesh(geometry, torusMaterial);
-      scene.add(mesh);
-    }
+					scene.add( group01 );
 
-    render();
+				});
+				
+				loader.load( './assets/img/opening/text02.svg', function ( paths ) {
 
-    //描画
-    function render() {
-      //player.position.z += 0.01;
+					var group02 = new THREE.Group();
+					group02.scale.multiplyScalar( 0.2 );
+					group02.position.x = 250;
+					group02.position.y = 250;
+					group02.position.z = 250;
+					group02.scale.y *= -1;
+					group02.rotation.y = 1.525;
 
-      // rendering
-      renderer.render(scene, camera);
+					for ( var i = 0; i < paths.length; i ++ ) {
 
-      // animation
-      requestAnimationFrame(render);
-    }
+						var path = paths[ i ];
 
+						var material = new THREE.MeshBasicMaterial( {
+							color: path.color,
+							side: THREE.DoubleSide,
+							depthWrite: false
+						} );
+
+						var shapes = path.toShapes( true );
+
+						for ( var j = 0; j < shapes.length; j ++ ) {
+
+							var shape = shapes[ j ];
+
+							var geometry = new THREE.ShapeBufferGeometry( shape );
+							var mesh = new THREE.Mesh( geometry, material );
+
+							group02.add( mesh );
+
+						}
+
+					}
+
+					scene.add( group02 );
+
+				});
+				
+				var cubeShader = THREE.ShaderLib[ "cube" ];
+				material = new THREE.MeshBasicMaterial( {
+					envMap: cubeCamera2.renderTarget.texture,
+					/*transparent: true,
+					opacity: 0.9,*/
+					side: THREE.BackSide
+				} );
+
+				sphere = new THREE.Mesh( new THREE.IcosahedronBufferGeometry( 20, 3 ), material );
+				scene.add( sphere );
+
+				/*cube = new THREE.Mesh( new THREE.BoxBufferGeometry( 20, 20, 20 ), material );
+				scene.add( cube );*/
+
+				//torus = new THREE.Mesh( new THREE.TorusKnotBufferGeometry( 10, 5, 100, 25 ), material );
+				//scene.add( torus );
+
+				//
+
+				//document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+				//document.addEventListener( 'wheel', onDocumentMouseWheel, false );
+
+				window.addEventListener( 'resize', onWindowResized, false );
+
+			}
+
+			function onWindowResized( event ) {
+
+				//renderer.setPixelRatio( window.devicePixelRatio );
+                renderer.setSize( window.innerWidth, window.innerHeight );
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+			}
+
+			/*function onDocumentMouseDown( event ) {
+
+				event.preventDefault();
+
+				onPointerDownPointerX = event.clientX;
+				onPointerDownPointerY = event.clientY;
+
+				onPointerDownLon = lon;
+				onPointerDownLat = lat;
+
+				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+				document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+
+			}
+
+			function onDocumentMouseMove( event ) {
+
+				lon = ( event.clientX - onPointerDownPointerX ) * 0.1 + onPointerDownLon;
+				lat = ( event.clientY - onPointerDownPointerY ) * 0.1 + onPointerDownLat;
+
+			}
+
+			function onDocumentMouseUp( event ) {
+
+				document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+				document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+
+			}
+
+			function onDocumentMouseWheel( event ) {
+
+				var fov = camera.fov + event.deltaY * 0.05;
+
+				camera.fov = THREE.Math.clamp( fov, 10, 75 );
+
+				camera.updateProjectionMatrix();
+
+			}*/
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+				render();
+
+			}
+
+			function render() {
+
+				var time = Date.now();
+
+				lon += .15;
+
+				lat = Math.max( - 85, Math.min( 85, lat ) );
+				phi = THREE.Math.degToRad( 90 - lat );
+				theta = THREE.Math.degToRad( lon );
+
+				/*cube.position.x = Math.cos( time * 0.001 ) * 30;
+				cube.position.y = Math.sin( time * 0.001 ) * 30;
+				cube.position.z = Math.sin( time * 0.001 ) * 30;
+
+				cube.rotation.x += 0.02;
+				cube.rotation.y += 0.03;*/
+
+				/*torus.position.x = Math.cos( time * 0.001 + 10 ) * 30;
+				torus.position.y = Math.sin( time * 0.001 + 10 ) * 30;
+				torus.position.z = Math.sin( time * 0.001 + 10 ) * 30;
+
+				torus.rotation.x += 0.02;
+				torus.rotation.y += 0.03;*/
+
+				camera.position.y = 100 * Math.cos( phi );
+				camera.position.x = 100 * Math.sin( phi ) * Math.cos( theta );
+				camera.position.z = 100 * Math.sin( phi ) * Math.sin( theta );
+
+				camera.lookAt( scene.position );
+
+				sphere.visible = false;
+
+				// pingpong
+
+				if ( count % 2 === 0 ) {
+
+					material.envMap = cubeCamera2.renderTarget.texture;
+					cubeCamera1.update( renderer, scene );
+
+				} else {
+
+					material.envMap = cubeCamera1.renderTarget.texture;
+					cubeCamera2.update( renderer, scene );
+
+				}
+
+				count ++;
+
+				sphere.visible = true;
+
+				renderer.render( scene, camera );
+
+			}
 
   }, false);
 })();
-
