39adb7e524a420ac0259b7975ece4745ba1b89b7
jshimazu <jshimazu0820@gmail.com>
2018-11-02 07:54:32 +0900

    カウンター修正

--- src-front/next100/src/views/Meeting.vue
+++ src-front/next100/src/views/Meeting.vue
@@ -43,9 +43,9 @@
                 <!-- TODO 時間 -->
                 <!--<div class="time-stamp">{{ moment(layer.created_at).format('H:m:s') }}</div>-->
                 <div class="number">
-                  <span class="numer">{{ layerIndex }}</span>
+                  <span class="numer">{{ layerIndex + 1 }}</span>
                   <span class="vinculum">/</span>
-                  <span class="denom">{{ layers.length - 1 }}</span></div>
+                  <span class="denom">{{ layers.length }}</span></div>
                 <div class="keyword-text">
                   <span>{{ layer.words.join(' + ') }}</span>
                 </div>
